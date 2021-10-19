bda676ee9ec16c5a7038874c75ac5c2dc93f357b
David Stern <20023188@tafe.wa.edu.au>
2021-10-19 12:56:53 +0800

    Update the UI automatically on a timer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from colours import Colours
+from threading import Timer
 
 
 def main():
@@ -14,12 +16,30 @@ def main():
     Button(window, text="Reset", width=25, height=2).grid(row=1, column=1)
     # Main colour
     Label(window, text="Main colour").grid(row=2, column=0, pady=20)
-    Label(window, font="Courier", text="#000000", bg="#000000", fg="#ffffff", width=12, height=2, pady=20).grid(row=3, column=0)
+    main_colour_label = Label(window, font="Courier", text="#000000", bg="#000000", fg="#ffffff", width=12, height=2, pady=20)
+    main_colour_label.grid(row=3, column=0)
     # Complimentary colour
     Label(window, text="Complementary colour", pady=20).grid(row=2, column=1)
-    Label(window, font="Courier", text="#ffffff", bg="#ffffff", fg="#000000", width=12, height=2, pady=20).grid(row=3, column=1)
+    comp_colour_label = Label(window, font="Courier", text="#ffffff", bg="#ffffff", fg="#000000", width=12, height=2, pady=20)
+    comp_colour_label.grid(row=3, column=1)
     # empty label for some extra spacing
     Label(window, text="").grid(row=4, column=0, columnspan=2)
+
+    # Create a random colour list
+    colours_list = Colours()
+
+    # Function to update the colours on a 1 second timer
+    def next_colours():
+        # Get the next colours
+        main_colour, comp_colour = colours_list.next()
+        # Update the main colour
+        main_colour_label.config(text=main_colour, bg=main_colour, fg=comp_colour)
+        # Update the complementary colour
+        comp_colour_label.config(text=comp_colour, bg=comp_colour, fg=main_colour)
+        Timer(1, next_colours).start()
+
+    # Start it off
+    next_colours()
 
     window.mainloop()
 
